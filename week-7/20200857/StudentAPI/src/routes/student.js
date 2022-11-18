3bd9b7fae9f3ec2cd7974851a257a72b15b08af9
BuiThucNam-20200857 <93958909+BuiThucNam@users.noreply.github.com>
2022-11-18 22:48:05 +0700

    Update student.js

--- week-7/20200857/StudentAPI/src/routes/student.js
+++ week-7/20200857/StudentAPI/src/routes/student.js
@@ -6,7 +6,7 @@ const route = express.Router();
 route.get('/students/:id', studentController.getStudent);
 route.get('/students', studentController.getAllStudents);
 route.put('/students/:id', studentController.editStudent);
-route.post('/students/create', studentController.addStudent);
+route.post('/students', studentController.addStudent);
 route.delete('/students/:id', studentController.deleteStudent);
 
 module.exports = route;
