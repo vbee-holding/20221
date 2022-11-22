9000b5f70e965c192dc7193be6150f46355c8234
Le Quoc Anh <leanh12082002@gmail.com>
2022-11-22 22:38:42 +0700

    Bai tap week7 quan ly sinh vien

---
+++ week-7/20204509/Nodejs-StudentServer/model/model.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const studentSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    mssv: {
+        type: String,
+        required: true
+    },
+    class: {
+        type: String,
+        required: true
+    },
+    course: {
+        type: Number,
+        required: true
+    },
+    email: {
+        type: String
+    },
+    phone: {
+        type: String
+    },
+    image: {
+        type: String,
+        default: "https://upanh.tv/image/BTlHEj"
+    },
+    address: {
+        type: String
+    }
+});
+
+let Student = mongoose.model("Student", studentSchema);
+module.exports = Student;
